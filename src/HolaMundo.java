9149f9ff503e9b6f286c310990ee05c457d4eb6d
wjsandy07 <wimar.sandy544@std.uni.edu.ni>
2025-06-21 16:23:17 -0600

    Tercer cambio (añadir multiplicacion)

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -5,8 +5,10 @@ public class HolaMundo {
         int a = 10;
         int b = 0;
         int suma = a + b;
+        int multiplicacion = a * b;
 
         System.out.println("Hola, " + nombre + "!");
         System.out.println("La suma de " + a + " + " + b + " es: " + suma);
+        System.out.println("La multiplicacion de " + a + " x " + b + " es: " + multiplicacion);
     }
 }
