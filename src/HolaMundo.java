70594b4727e84ea27af3637a58df8abd96b09b8a
wjsandy07 <wimar.sandy544@std.uni.edu.ni>
2025-06-21 15:30:39 -0600

    Creacion del proyecto

---
+++ src/HolaMundo.java
@@ -0,0 +1,12 @@
+public class HolaMundo {
+    public static void main(String[] args) throws Exception {
+        
+        String nombre = "Estudiante";
+        int a = 5;
+        int b = 3;
+        int suma = a + b;
+
+        System.out.println("Hola, " + nombre + "!");
+        System.out.println("La suma de " + a + " + " + b + " es: " + suma);
+    }
+}
