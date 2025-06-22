dde33d5b2eb4bd4272774e00b4bad9525ba48fa4
wjsandy07 <wimar.sandy544@std.uni.edu.ni>
2025-06-21 20:32:37 -0600

    cambio saludos

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -10,5 +10,6 @@ public class HolaMundo {
         System.out.println("Hola, " + nombre + "!");
         System.out.println("La suma de " + a + " + " + b + " es: " + suma);
         System.out.println("La multiplicacion de " + a + " x " + b + " es: " + multiplicacion);
+        System.out.println("Wimar Jair Sandy Zambrana - 2025-1611U");
     }
 }
